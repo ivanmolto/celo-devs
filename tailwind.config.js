83320b53049a1e7483d6e288fee26b351ce83d7a
Ivan Molto <ivanmolto@gmail.com>
2022-07-16 23:39:29 +0200

    chore: Customize fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      garamond: ["EB Garamond", "serif"],
+      jost: ["Jost", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "celo-green": "#35D07F",
+        "celo-yellow": "#FBCC5C",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
