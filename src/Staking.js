effd550f4e99ac4c779b71a3bc9490edaa4448c7
Ivan Molto <ivanmolto@gmail.com>
2022-07-17 11:22:09 +0200

    feat: Add more Staking companies

--- src/Staking.js
+++ src/Staking.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { ArchiveIcon, GlobeAltIcon } from "@heroicons/react/solid";
 
 const projects = [
@@ -7,7 +8,23 @@ const projects = [
     link: "https://steakwallet.fi",
     description: "Multi-chain DeFi wallet",
     tags: "non-custodial",
-    repos: "17",
+    repos: [
+      { name: "assets" },
+      { name: "walletconnect-docs" },
+      { name: "wallet-adapter" },
+      { name: "rainbowkit" },
+      { name: "cosmos-wallet" },
+      { name: "web-examples" },
+      { name: "airdrops" },
+      { name: "encrypt-decrypt" },
+      { name: "react-native-reanimated" },
+      { name: "steakwallet.fi" },
+      { name: "solido.js" },
+      { name: "walletconnect-registry" },
+      { name: "default-token-list" },
+      { name: "interface" },
+      { name: "Steakwallet-Art" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/82271623?s=200&v=4",
   },
   {
@@ -16,7 +33,20 @@ const projects = [
     link: "https://buidllabs.io",
     description: "Building infrastructure for the new decentralized world",
     tags: "non-custodial",
-    repos: "50",
+    repos: [
+      { name: "yieldscan-backend" },
+      { name: "cvvt-frontend" },
+      { name: "celo-indexer" },
+      { name: "churrofi-twitter-bot" },
+      { name: "churrofi-reminder" },
+      { name: "foamviz" },
+      { name: "celo-voting-validator-backend" },
+      { name: "YieldScan" },
+      { name: "celo-onchain-data-service" },
+      { name: "cryptoverse-wars-backend" },
+      { name: "wallet" },
+      { name: "celo-vg-voting-tool-frontend" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/48378009?s=200&v=4",
   },
   {
@@ -25,7 +55,13 @@ const projects = [
     link: "https://docs.savingscelo.com",
     description: "",
     tags: "defi",
-    repos: "5",
+    repos: [
+      { name: "swappa" },
+      { name: "celo-devchain" },
+      { name: "savingscelo-with-ube" },
+      { name: "savingscelo-cli" },
+      { name: "savingscelo" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/92624772?s=200&v=4",
   },
   {
@@ -34,7 +70,7 @@ const projects = [
     link: "https://rcelo.studio/stake",
     description: "",
     tags: "defi",
-    repos: "2",
+    repos: [{ name: "romulus-interface" }, { name: "romulus" }],
     imageUrl: "https://avatars.githubusercontent.com/u/84882432?s=200&v=4",
   },
   {
@@ -43,16 +79,39 @@ const projects = [
     link: "https://celowallet.app/setup",
     description: "A home for community-driven tools for the Celo ecosystem",
     tags: "desktop",
-    repos: "3",
+    repos: [
+      { name: "celo-ethers-wrapper" },
+      { name: "celo-web-wallet" },
+      { name: "mento-fi" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/70605918?s=200&v=4",
   },
   {
     name: "Zviad Metreveli",
     handle: "zviadm",
-    link: "https://www.linkedin.com/in/zviad-metreveli-12234328/",
+    link: "https://www.linkedin.com/in/zviad-metreveli-12234328",
     description: "",
     tags: "web",
-    repos: "28",
+    repos: [
+      { name: "celoterminal" },
+      { name: "walletconnect-registry" },
+      { name: "celovote-app" },
+      { name: "default-token-list" },
+      { name: "celogovernance" },
+      { name: "CIPs" },
+      { name: "crypto-ecosystems" },
+      { name: "celo-monorepo" },
+      { name: "celo-hellocontracts" },
+      { name: "celo-blockchain" },
+      { name: "walle" },
+      { name: "wt" },
+      { name: "tt" },
+      { name: "stats-go" },
+      { name: "zlog" },
+      { name: "react-native-camera" },
+      { name: "grpc-go" },
+      { name: "gocql" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/517937?v=4",
   },
   {
@@ -61,16 +120,39 @@ const projects = [
     link: "https://coinlist.co",
     description: "",
     tags: "custodial",
-    repos: "4",
+    repos: [
+      { name: "cap-ec2" },
+      { name: "scale.rb" },
+      { name: "sinatra-base" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/31194466?s=200&v=4",
   },
   {
     name: "Coinbase",
-    handle: "masa-finance",
+    handle: "coinbase",
     link: "https://coinbase.com/custody",
     description: "",
     tags: "custodial",
-    repos: "104",
+    repos: [
+      { name: "rest-hooks" },
+      { name: "coinbase-wallet-sdk" },
+      { name: "salus" },
+      { name: "rosetta-cli" },
+      { name: "rosetta-geth-sdk" },
+      { name: "rosetta-sdk-go" },
+      { name: "rosetta-specifications" },
+      { name: "kryptology" },
+      { name: "rosetta-bitcoin" },
+      { name: "nft-dapp-starter-kit" },
+      { name: "cbpay-js" },
+      { name: "coinbase-commerce-node" },
+      { name: "react-coinbase-commerce" },
+      { name: "rosetta-ecosystem" },
+      { name: "coinbase-commerce-woocommerce" },
+      { name: "coinbase-commerce-php" },
+      { name: "mkr-vote-proxy" },
+      { name: "coinbase-ios-sdk" },
+    ],
     imageUrl: "https://avatars.githubusercontent.com/u/1885080?s=200&v=4",
   },
 ];
@@ -87,7 +169,7 @@ const Staking = () => {
             <img
               className="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
               src={project.imageUrl}
-              alt={project.name}
+              alt=""
             />
             <h3 className="mt-6 text-white text-base font-medium font-jost">
               {project.name}
@@ -106,27 +188,26 @@ const Staking = () => {
           <div>
             <div className="-mt-px flex divide-x divide-gray-200">
               <div className="w-0 flex-1 flex">
-                <a
-                  href={`mailto:${project.email}`}
+                <Link
+                  to={`/repos/${project.handle}`}
+                  state={project.repos}
                   className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
                 >
                   <ArchiveIcon
                     className="w-5 h-5 text-gray-400"
                     aria-hidden="true"
                   />
-                  <span className="ml-3 text-gray-200">{project.repos}</span>
-                </a>
+                </Link>
               </div>
               <div className="-ml-px w-0 flex-1 flex">
                 <a
-                  href={`tel:${project.link}`}
+                  href={`${project.link}`}
                   className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
                 >
                   <GlobeAltIcon
                     className="w-5 h-5 text-gray-400"
                     aria-hidden="true"
                   />
-                  <span className="ml-3 text-gray-200">{project.people}</span>
                 </a>
               </div>
             </div>
