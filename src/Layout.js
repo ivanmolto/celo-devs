68cd1b79d4d9cc651b97fbcb3a0f22254c742cab
Ivan Molto <ivanmolto@gmail.com>
2022-07-17 17:49:56 +0200

    feat: Add logo navigation

--- src/Layout.js
+++ src/Layout.js
@@ -97,7 +97,9 @@ const Layout = () => {
                     </div>
                   </Transition.Child>
                   <div className="flex-shrink-0 flex items-center px-4">
-                    <img src={celodevs} alt="Celo Devs" />
+                    <Link to="/">
+                      <img src={celodevs} alt="Celo Devs" />
+                    </Link>
                   </div>
                   <nav
                     className="mt-5 flex-shrink-0 h-full divide-y divide-green-800 overflow-y-auto"
@@ -175,7 +177,7 @@ const Layout = () => {
                             className="mr-4 flex-shrink-0 h-6 w-6 text-white"
                             aria-hidden="true"
                           />
-                          <span className="text-base">Home</span>
+                          <span className="text-sm">Home</span>
                         </CustomLink>
                         <a
                           key="GitHub"
@@ -186,7 +188,7 @@ const Layout = () => {
                             className="mr-4 h-6 w-6 text-white"
                             aria-hidden="true"
                           />
-                          <span className="text-base">Repo</span>
+                          <span className="text-sm">Repo</span>
                         </a>
                       </div>
                     </div>
@@ -201,7 +203,9 @@ const Layout = () => {
         <div className="hidden lg:flex lg:w-64 lg:flex-col lg:fixed lg:inset-y-0">
           <div className="flex flex-col flex-grow bg-celo-green pt-5 pb-4 overflow-y-auto ">
             <div className="flex items-center flex-shrink-0 px-4">
-              <img src={celodevs} alt="Celo Devs" />
+              <Link to="/">
+                <img src={celodevs} alt="Celo Devs" />
+              </Link>
             </div>
             <nav
               className="mt-5 flex-1 flex flex-col divide-y divide-green-800 overflow-y-auto"
